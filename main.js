2f81bf98aa8e6ba802f5589863691676f0b364be
AhmedKaram50 <ahmeadkaram2018@gmail.com>
2024-01-30 00:38:20 +0200

    Question 1

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
-import './style.css'
+import "./style.css";
+
+const btn = document.getElementById("once");
+const onceResultDiv = document.getElementById("once_result");
+const toggler = document.getElementById("toggler");
+const togglerResult = document.getElementById("toggler_result");
+
 
 /*
-    * once() => it takes a function and execute that function only once
-*/
-const once = fn => {
-  let finished = false
+ * once() => it takes a function and execute that function only once
+ */
+
+export const once = (fn) => {
   return (...args) => {
-      if (!finished) {
-          finished = true
-          fn(...args)
-      }
-  }
-}
+    if (fn != null) {
+      fn(...args);
+      fn = null;
+    }
+  };
+};
 
-const btn = document.getElementById('once')
-const onceResultDiv = document.getElementById('once_result')
 
-const addHtmlOnce = once(addHTMLToElement)
-btn.addEventListener('click', () => addHtmlOnce(onceResultDiv, '<p>Result One Time</p>'))
+const addHtmlOnce = once(addHTMLToElement);
+btn.addEventListener("click", () =>
+  addHtmlOnce(onceResultDiv, "<p>Result One Time</p>")
+);
 
 function addHTMLToElement(el, data) {
-  el.insertAdjacentHTML('beforeend', data)
+  el.insertAdjacentHTML("beforeend", data);
 }
 
+/*
+ * alternator() => it takes two functions and when it called multiple time it switches between the functions
+ */
+
+const alternator = (fn1, fn2) => {
+  let toggler = false;
+  return () => {
+    !toggler ? fn1() : fn2()
+    toggler = !toggler
+  };
+};
+
+const fn1 = () => addHTMLToElement(onceResultDiv, '<p>Fn1 Called</p>');
+const fn2 = () => addHTMLToElement(onceResultDiv, '<p>Fn2 Called</p>');
+
+const alt = alternator(fn1, fn2);
+toggler.addEventListener('click', () => alt())
